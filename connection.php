42602c546d4c5dbec2d1edcc8cad793376c72a7a
Sasha Slutsker <slutsker@umd.edu>
2009-06-14 20:31:45 -0700

    Deleting unnecessary file

--- connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $serv = 'http://localhost/personal/';
-  $dbh = mysql_connect('localhost','woo','pass') or die (mysql_error());
-  @mysql_select_db('sashaslutsker') or die( "Unable to select database");
-?>
